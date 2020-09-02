5609d57f258223f33d6b544e348d6bdbd6329c59
Aashir Sohail <asohial.bscs16seecs@seecs.edu.pk>
2020-09-03 03:12:31 +0500

    built the static assests

---
+++ build/precache-manifest.4e2856f424e5e08cb859782b411a3168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7817c258d55432891ca98bcce5159a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97febe3c8ed55e3dc4cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e50995a436f91f8455fd",
+    "url": "/static/js/2.9eacc742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9eacc742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97febe3c8ed55e3dc4cf",
+    "url": "/static/js/main.745bfef4.chunk.js"
+  },
+  {
+    "revision": "cda5c82ad5afc92c5a93",
+    "url": "/static/js/runtime-main.2870b264.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
